d93c7651ca4a26d8cd9abc97ea4e9aee4aaa1494
X <xavier@learn2earnfresno.org>
2019-02-27 22:25:03 -0800

    113. Made in line with standard

--- src/main/webapp/app/shared/@theme/layouts/content/content-layout.component.ts
+++ src/main/webapp/app/shared/@theme/layouts/content/content-layout.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-    selector: 'app-content-layout',
+    selector: 'jhi-content-layout',
     templateUrl: './content-layout.component.html',
     styleUrls: ['./content-layout.component.scss']
 })
