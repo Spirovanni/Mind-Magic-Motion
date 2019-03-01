df5825cc5c026a1b33c0679cc9be6c2c6035b89d
X <xavier@learn2earnfresno.org>
2019-03-01 14:03:46 -0800

    127. Updated the index file for the layouts in theme folder

--- src/main/webapp/app/shared/@theme/layouts/index.ts
+++ src/main/webapp/app/shared/@theme/layouts/index.ts
@@ -1,5 +1,3 @@
 export * from './one-column/one-column.layout';
 export * from './two-columns/two-columns.layout';
 export * from './three-columns/three-columns.layout';
-export * from './content/content-layout.component';
-export * from './full/full-layout.component';
