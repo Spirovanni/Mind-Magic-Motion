a4519b73c6e0ad5f471cc5e88cbfc8014247245e
X <xavier@learn2earnfresno.org>
2019-02-28 22:00:32 -0800

    123. Added guild-my-suffix.model.ts

---
+++ src/main/webapp/app/shared/models/guild-my-suffix.model.ts
@@ -0,0 +1,29 @@
+import { IPlayerMySuffix } from 'app/shared/model//player-my-suffix.model';
+
+export interface IGuildMySuffix {
+    id?: number;
+    guildImageContentType?: string;
+    guildImage?: any;
+    guildName?: string;
+    streetAddress?: string;
+    postalCode?: string;
+    city?: string;
+    region?: string;
+    stateProvince?: string;
+    players?: IPlayerMySuffix[];
+}
+
+export class GuildMySuffix implements IGuildMySuffix {
+    constructor(
+        public id?: number,
+        public guildImageContentType?: string,
+        public guildImage?: any,
+        public guildName?: string,
+        public streetAddress?: string,
+        public postalCode?: string,
+        public city?: string,
+        public region?: string,
+        public stateProvince?: string,
+        public players?: IPlayerMySuffix[]
+    ) {}
+}
